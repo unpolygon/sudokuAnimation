022eb90c37db53d248c628b56541afccb9569cbe
Unpolygon <saengsetth@outlook.com>
2020-01-09 17:16:27 +0700

    Updates

---
+++ precache-manifest.4789bd829ddfd7058fe8a9db4e09c0d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dad43a5d1965ff0ccf9477dfd54955fe",
+    "url": "/sudokuAnimation/index.html"
+  },
+  {
+    "revision": "ac5d63781c765dd3f78f",
+    "url": "/sudokuAnimation/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "3041889baf7e3e46d2ed",
+    "url": "/sudokuAnimation/static/css/main.5ac8bdc3.chunk.css"
+  },
+  {
+    "revision": "ac5d63781c765dd3f78f",
+    "url": "/sudokuAnimation/static/js/2.87c2ec73.chunk.js"
+  },
+  {
+    "revision": "3041889baf7e3e46d2ed",
+    "url": "/sudokuAnimation/static/js/main.f5e08991.chunk.js"
+  },
+  {
+    "revision": "9ab32e9c566e4364ad62",
+    "url": "/sudokuAnimation/static/js/runtime-main.1a82fee4.js"
+  }
+]);
